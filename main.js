e409b7dbd028e0085c469f4160f5f6e129461b83
sniller27 <sniller27@hotmail.com>
2016-11-02 23:52:00 +0100

    states, properties and multiple components

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
+//imports
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
+//plugin fix. Needed for onTouchTap. (temporary)
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
+
+import ReactDOM from 'react-dom';
+//for providing a theme
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyButtons from './MyComponent.jsx';
+//import MyButtons from './MyComponent.jsx';
+//import MyMenu from './MyComponent2.jsx';
+
+//plugin fix. Needed for onTouchTap. (temporary)
+injectTapEventPlugin();
+
+//theme object?
+const App = () => (
+  <MuiThemeProvider>
+    <MyButtons />
+  </MuiThemeProvider>
+);
+
+//Rendering to virtual DOM?
 ReactDOM.render(<App />, document.getElementById('app'));
